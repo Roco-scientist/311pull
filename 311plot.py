3cc5cbebceffb5d35ad3a3dca79258568398c30a
Rory Coffey <coffeyrt@gmail.com>
2022-08-22 19:57:52 -0400

    added arguments and saving of html

--- 311plot.py
+++ 311plot.py
@@ -7,26 +7,27 @@ import statistics
 from sqlite3 import Error
 from pandas import DataFrame
 from datetime import datetime
-
-DATABASE = "../311.db"
+from pathlib import Path
 
 
 def arguments():
     parser = argparse.ArgumentParser(description="")
-    parser.add_argument("--database", help="")
+    parser.add_argument("--database", help="Input sqlite3 db")
+    parser.add_argument("--out", help="Output directory")
     return parser.parse_args()
 
 
-def get_connection():
+def get_connection(database: str):
     conn = None
     try:
-        conn = sqlite3.connect(DATABASE)
+        conn = sqlite3.connect(database)
     except Error as e:
         print(e)
     return conn
 
 
-def plot(data: DataFrame) -> None:
+def plot(data: DataFrame, out_dir: str) -> None:
+    out_path = Path(out_dir)
     center_lon = statistics.median(data.longitude)
     center_lat = statistics.median(data.latitude)
     frame_week = []
@@ -55,20 +56,35 @@ def plot(data: DataFrame) -> None:
         mapbox_style="stamen-terrain",
         animation_frame="Month",
     )
-    fig.show()
+    # fig.show()
+    fig.write_html(out_path / "311_month.html")
+    fig = px.density_mapbox(
+        data,
+        lat="latitude",
+        lon="longitude",
+        z="Quantity",
+        radius=10,
+        center=dict(lat=center_lat, lon=center_lon),
+        zoom=11,
+        mapbox_style="stamen-terrain",
+        animation_frame="Week",
+    )
+    fig.write_html(out_path / "311_week.html")
 
 
 def main():
-    conn = get_connection()
-    cur = conn.cursor()
-    cur.execute(
-        "SELECT latitude,longitude,opened FROM cases WHERE title LIKE '%Needle%'"
-    )
-    data = cur.fetchall()
-    df = DataFrame(data=data, columns=["latitude", "longitude", "opened"])
-    df["Quantity"] = 1
-    conn.close()
-    plot(df)
+    args = arguments()
+    conn = get_connection(args.database)
+    if conn is not None:
+        cur = conn.cursor()
+        cur.execute(
+            "SELECT latitude,longitude,opened FROM cases WHERE title LIKE '%Needle%'"
+        )
+        data = cur.fetchall()
+        df = DataFrame(data=data, columns=["latitude", "longitude", "opened"])
+        df["Quantity"] = 1
+        conn.close()
+        plot(df, args.out)
 
 
 if __name__ == "__main__":
