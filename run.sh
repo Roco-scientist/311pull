0630467ec85911ccf9c8af4dd78aae74d0a7ef62
Rory Coffey <coffeyrt@gmail.com>
2022-08-31 06:37:14 -0400

    updated database

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 timeout 43200 /usr/bin/python /home/coffeyrt/311pull/311pull.py
-/usr/bin/python /home/coffeyrt/311pull/311plot.py --database /media/main/311.db --out /media/main/stock/
+/usr/bin/python /home/coffeyrt/311pull/311plot.py --database /media/main/Rory/311/311.db --out /media/main/Rory/311/
